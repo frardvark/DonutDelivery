6a85ddb8007f0fb99d79293b2dfc12cca199d5d4
Julian <frardvark@gmail.com>
2019-01-29 18:20:49 -0500

    Truck scene?

---
+++ Donut Delivery/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3(horizontalAxis * speed, 0f, verticalAxis * speed), ForceMode.Impulse);
+    }
+}
