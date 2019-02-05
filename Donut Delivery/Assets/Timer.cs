d96f499355bd3aed1379e679a122c7bf781329f5
psanchez1 <psanchez1@ufl.edu>
2019-02-05 13:38:28 -0500

    time value change

--- Donut Delivery/Assets/Timer.cs
+++ Donut Delivery/Assets/Timer.cs
@@ -22,11 +22,11 @@ public class Timer : MonoBehaviour
     {
         checkTime -= Time.deltaTime;
 
-        //Text will change to yellow when timer < 50% and to red when timer < 25%
-        if (checkTime <= (TimeLimit * 0.5f) && checkTime > (TimeLimit * 0.25f))
+        //Text will change to yellow when timer < 50% and to red when timer < 20%
+        if (checkTime <= (TimeLimit * 0.5f) && checkTime > (TimeLimit * 0.20f))
             timerText.color = Color.yellow;
         
-        if (checkTime <= (TimeLimit * 0.25f))
+        if (checkTime <= (TimeLimit * 0.20f))
             timerText.color = Color.red;
         
 
