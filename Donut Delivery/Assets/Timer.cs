c6d009c8d5f2c77abcc0c3b029cb392596599792
psanchez1 <psanchez1@ufl.edu>
2019-02-05 13:09:24 -0500

    create UI Text object and Timer script

---
+++ Donut Delivery/Assets/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLimit = 120f;
+    private float checkTime;
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkTime = TimeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkTime -= Time.deltaTime;
+        timerText.text = "Time Left: " + checkTime.ToString("n2") + " seconds";
+    }
+}
