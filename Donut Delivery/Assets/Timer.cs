844d946f5994c1aca05b60d932f6744b23ed72e9
psanchez1 <psanchez1@ufl.edu>
2019-02-05 13:19:14 -0500

    timer stops when complete now

--- Donut Delivery/Assets/Timer.cs
+++ Donut Delivery/Assets/Timer.cs
@@ -5,20 +5,29 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float TimeLimit = 120f;
+    public float TimeLimit = 10f;
     private float checkTime;
     public Text timerText;
+    private bool stop;
 
     // Start is called before the first frame update
     void Start()
     {
         checkTime = TimeLimit;
+        stop = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         checkTime -= Time.deltaTime;
-        timerText.text = "Time Left: " + checkTime.ToString("n2") + " seconds";
+        if (checkTime <= 0.00f)
+            stop = true;
+        
+        if (!stop)
+            timerText.text = "Time Left: " + checkTime.ToString("n2") + " seconds";
+        else
+            timerText.text = "Time Left: 0.00";
+        
     }
 }
