1f8ffef9cafe7e89c2051c58e83589a29b7b90e2
psanchez1 <psanchez1@ufl.edu>
2019-02-11 19:43:09 -0500

    victory screen

--- Donut Delivery/Assets/Scripts/Timer.cs
+++ Donut Delivery/Assets/Scripts/Timer.cs
@@ -9,17 +9,22 @@ public class Timer : MonoBehaviour
     public float TimeLimit = 10f;
     private float checkTime;
     public int houses;
+    public int totalHouses;
     public Text timerText;
     public Text message;
     public Text counter;
     private bool stop;
+    private bool lost;
 
     // Start is called before the first frame update
     void Start()
     {
+        lost = false;
         checkTime = TimeLimit;
         stop = false;
         houses = 0;
+        var deliveryTargets = GameObject.FindGameObjectsWithTag("DeliveryTarget");
+        totalHouses = deliveryTargets.Length;
         counter.text = "Donuts Delivered: " + houses;
     }
 
@@ -28,6 +33,13 @@ public class Timer : MonoBehaviour
     {
         counter.text = "Donuts Delivered: " + houses;
         checkTime -= Time.deltaTime;
+
+        if (houses == totalHouses && !lost)
+        {
+            stop = true;
+            message.text = "All donuts delivered on time! Congratulations!";
+        }
+        
 
         //Text will change to yellow when timer < 50% and to red when timer < 20%
         if (checkTime <= (TimeLimit * 0.5f) && checkTime > (TimeLimit * 0.20f))
@@ -40,6 +52,7 @@ public class Timer : MonoBehaviour
         if (checkTime <= 0.00f)
         {
             stop = true;
+            lost = true;
             message.text = "Time's Up! Try Again? \n       Press Space";
 
             //press spacebar to restart level
